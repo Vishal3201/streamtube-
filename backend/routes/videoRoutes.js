7f4cc4822eaedb161dd7673f5c02b170ee1e7cd5
Vishal Saroj <vishalsaroj3201@gmail.com>
2026-04-02 01:06:49 +0530

    Update videoRoutes.js

--- backend/routes/videoRoutes.js
+++ backend/routes/videoRoutes.js
@@ -1,30 +1,10 @@
 import express from "express";
-import multer from "multer";
-import {
-  getVideos,
-  getVideoById,
-  uploadVideo
-} from "../controllers/videoController.js";
+import { uploadVideo, getVideos } from "../controllers/videoController.js";
 
 const router = express.Router();
 
-// ✅ multer config
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
-});
 
-const upload = multer({ storage });
-
-// ✅ routes
+router.post("/upload", uploadVideo);
 router.get("/", getVideos);
-router.get("/:id", getVideoById);
-
-// 🔥 upload route
-router.post("/", upload.single("video"), uploadVideo);
 
 export default router;
